bdcb82971324e9bfc598582ca2ceb95eae33cbc2
amk <amk>
1998-10-07 02:06:21 +0000

    Change some node type constants to the _NODE form.

--- xml/dom/walker.py
+++ xml/dom/walker.py
@@ -3,15 +3,15 @@ from xml.dom.core import *
 class Walker:
 
 	def walk(self, root):
-		if root.get_nodeType() == DOCUMENT:
+		if root.get_nodeType() == DOCUMENT_NODE:
 			c = root.documentElement
-			assert c.get_nodeType() == ELEMENT
+			assert c.get_nodeType() == ELEMENT_NODE
 			return self.walk1(c)
 		else:
 			return self.walk1(root)
 		
 	def walk1(self, node):
-		if node.get_nodeType() == ELEMENT:
+		if node.get_nodeType() == ELEMENT_NODE:
 			self.startElement(node)
 			for child in node.get_childNodes():
 				self.walk1(child)
