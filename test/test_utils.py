c0dc7525486014c519068aa8a2785929faebfd8f
amk <amk>
1998-10-19 03:20:53 +0000

    Added a test of the iso8601 module

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,8 +5,27 @@ from xml.utils import *
 print 'Testing utils.escape'
 print 'These pairs of strings should all be identical'
 
-print escape('&<>'), '&amp;&lt;&gt;'
-print escape('foo&amp;bar'), 'foo&amp;amp;bar'
-print escape('< test > &', {'test': '&myentity;'}), '&lt; &myentity; &gt; &amp;'
+v1, v2 = escape('&<>'), '&amp;&lt;&gt;'
+print v1 == v2, repr(v1), repr(v2)
+v1, v2 = escape('foo&amp;bar'), 'foo&amp;amp;bar'
+print v1 == v2, repr(v1), repr(v2)
+v1, v2 = escape('< test > &', {'test': '&myentity;'}), '&lt; &myentity; &gt; &amp;'
+print v1 == v2, repr(v1), repr(v2)
+v1, v2 = escape('&\'"<>', {'"': '&quot;', "'": '&apos;'}), '&amp;&apos;&quot;&lt;&gt;'
+print v1 == v2, repr(v1), repr(v2)
 
-# XXX add test suite for is-8601
+# Test the iso8601 module
+import time
+saved_timezone = time.timezone
+time.timezone = 0
+
+for dt in ['1998', '1998-06', '1998-06-13', 
+           '1998-06-13T14:12Z',
+           '1998-06-13T14:12:30Z', 
+           '1998-06-13T14:12:30.2Z'
+           ]:
+    date = iso8601.parse( dt )
+    print iso8601.tostring( date )
+        
+time.timezone = saved_timezone 
+
