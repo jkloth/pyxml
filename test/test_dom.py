b2113b41bcd695edde82256c9adffe1936543986
amk <amk>
1998-12-03 01:49:33 +0000

    Made the DOM test work again

--- test/test_dom.py
+++ test/test_dom.py
@@ -1,40 +1,28 @@
-from xml.dom.transformer import *
-from xml.dom.writer import HtmlWriter
 
-test_text = """<?xml>
+import StringIO
+from xml.dom import core, sax_builder
+from xml.sax import saxexts
+
+test_text = """<?xml version="1.0"?>
 <doc>
 <title>This is a test</title>
-
 <h1>Don't panic</h1>
-
 <p>Maybe it will work.</p>
-
-<h2>We can handle it</>
-
+<h2>We can handle it</h2>
 <h3>Yes we can</h3>
 <h3>Or maybe not</h3>
-
 End of test.
 </doc>
 """
 
+p = saxexts.make_parser()
+h = sax_builder.SaxBuilder()
+p.setDocumentHandler( h )
+file = StringIO.StringIO( test_text )
+p.parseFile( file )
 
-class TestTransformer(Transformer):
-	def do_doc(self, node):
-		return [HTML(
-			HEAD(
-				TITLE(cdata(having(node._children, 'this.GI == "title"')))
-			),
-			BODY(
-				{'bgcolor': '#FFFFFF', 'text': '#000000'},
-				having(node._children, 'this.GI in ["h1", "h2", "h3"]')
-			)
-		)]
+doc = h.document
+print doc.toxml()
 
-transformer = TestTransformer()
-parser = DcBuilder()
-parser.feed(test_text)
-document = transformer.transform(parser.document)
 
-writer = HtmlWriter()
-writer.write(document)
+
