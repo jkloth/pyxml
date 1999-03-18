15ca759aa527f3c3e153607da560e55f98bd3cdb
amk <amk>
1999-03-18 12:38:28 +0000

    Reindented files to 4-space indents

--- dom/builder.py
+++ dom/builder.py
@@ -8,71 +8,71 @@ from xml.dom.core import *
 import string
 
 _LEGAL_DOCUMENT_CHILDREN = (ELEMENT_NODE, PROCESSING_INSTRUCTION_NODE,
-			    COMMENT_NODE)
+                COMMENT_NODE)
 
 
 class Builder:
 
-	def __init__(self):
-		self.document = createDocument()
-		self.current_element = None
+    def __init__(self):
+        self.document = createDocument()
+        self.current_element = None
 
 
-	def push(self, node):
-		"Add node to current node and move to new node."
+    def push(self, node):
+        "Add node to current node and move to new node."
 
-		nodetype = node.get_nodeType()
-		if self.current_element:
-			self.current_element.insertBefore(node, None)
-		elif nodetype in _LEGAL_DOCUMENT_CHILDREN:
-			if nodetype == TEXT_NODE:
-				if string.strip(node.get_nodeValue()) != "":
-					self.document.appendChild(node)
-			else:
-				self.document.appendChild(node)
+        nodetype = node.get_nodeType()
+        if self.current_element:
+            self.current_element.insertBefore(node, None)
+        elif nodetype in _LEGAL_DOCUMENT_CHILDREN:
+            if nodetype == TEXT_NODE:
+                if string.strip(node.get_nodeValue()) != "":
+                    self.document.appendChild(node)
+            else:
+                self.document.appendChild(node)
 
-		if nodetype == ELEMENT_NODE:
-			self.current_element = node
-		
-	def pop(self):
-		"Move to current node's parent."
+        if nodetype == ELEMENT_NODE:
+            self.current_element = node
+        
+    def pop(self):
+        "Move to current node's parent."
 
-		self.current_element = self.current_element.get_parentNode()
-		
+        self.current_element = self.current_element.get_parentNode()
+        
 
-	def startElement(self, name, attrs = {}):
-		if hasattr(self, 'start_' + name):
-			getattr(self, 'start_' + name)(name, attrs)
-		else:
-			element = self.document.createElement(name)
-			for key, value in attrs.items():
-				element.setAttribute(key, value)
-			self.push(element)
-	
-	def endElement(self, name):
-		assert name == self.current_element.get_nodeName()
-		self.pop()
+    def startElement(self, name, attrs = {}):
+        if hasattr(self, 'start_' + name):
+            getattr(self, 'start_' + name)(name, attrs)
+        else:
+            element = self.document.createElement(name)
+            for key, value in attrs.items():
+                element.setAttribute(key, value)
+            self.push(element)
+    
+    def endElement(self, name):
+        assert name == self.current_element.get_nodeName()
+        self.pop()
 
-	def comment(self,s):
-		if self.current_element:
-			comment_node = self.document.createComment(s)
-			self.current_element.insertBefore(comment_node, None)
-	
-	def processingInstruction(self, target, data):
-		node = self.document.createProcessingInstruction(target, data)
-		self.push(node)
+    def comment(self,s):
+        if self.current_element:
+            comment_node = self.document.createComment(s)
+            self.current_element.insertBefore(comment_node, None)
+    
+    def processingInstruction(self, target, data):
+        node = self.document.createProcessingInstruction(target, data)
+        self.push(node)
 
-	def entityref(self, name):
-		node = self.document.createEntityReference(name)
-	        self.push(node)
+    def entityref(self, name):
+        node = self.document.createEntityReference(name)
+        self.push(node)
 
-	def text(self, s):
-		if self.current_element:
-			text_node = self.document.createTextNode(s)
-			if (self.current_element == self.document and
-			    string.strip(s) == ""):
-				return
+    def text(self, s):
+        if self.current_element:
+            text_node = self.document.createTextNode(s)
+            if (self.current_element == self.document and
+                string.strip(s) == ""):
+                return
 
-			self.current_element.insertBefore(text_node, None)
+            self.current_element.insertBefore(text_node, None)
 
 
--- xml/dom/transform.py
+++ xml/dom/transform.py
@@ -1,38 +1,32 @@
 from xml.dom.core import *
 
 def explode(nodes, condition):
-	for node in nodes:
-		explode_node(node, condition)
+    for node in nodes:
+        explode_node(node, condition)
 
-	return nodes
+    return nodes
 
 def explode_node(node, condition):
-	if node.NodeType != ELEMENT:
-		return node
+    if node.NodeType != ELEMENT:
+        return node
 
-	explode(node.getChildren(), condition)
+    explode(node.getChildren(), condition)
 
-	if eval(condition, {}, {'this': node}):
-		parent = node.getParentNode()
-		for child in node.getChildren():
-			parent.insertBefore(child, node)
-		parent.removeChild(node)
+    if eval(condition, {}, {'this': node}):
+        parent = node.getParentNode()
+        for child in node.getChildren():
+            parent.insertBefore(child, node)
+        parent.removeChild(node)
 
-	return node
-		
-		
+    return node
+        
+        
 if __name__ == '__main__':
-	from xml.dom.writer import HtmlWriter
+    from xml.dom.writer import HtmlWriter
 
-	tree = HTML(BODY(H1(A({'href':'blah'}, 'blah blah'))))
-	w = HtmlWriter()
-	w.write(tree)
+    tree = HTML(BODY(H1(A({'href':'blah'}, 'blah blah'))))
+    w = HtmlWriter()
+    w.write(tree)
 
-	explode_node(tree, 'this.GI == "A"')
-	w.write(tree)
-
-
-
-			
-			
-
+    explode_node(tree, 'this.GI == "A"')
+    w.write(tree)
