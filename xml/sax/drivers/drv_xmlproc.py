3100bdcf5fb4c423edf283678b6cc5dcda5d98b7
lars <lars>
2000-05-12 18:41:06 +0000

    Committed modified drivers for xmlproc 0.70.

--- xml/sax/drivers/drv_xmlproc.py
+++ xml/sax/drivers/drv_xmlproc.py
@@ -1,13 +1,31 @@
 """
 A SAX driver for xmlproc
 
-$Id: drv_xmlproc.py,v 1.8 1999/08/15 23:53:07 amk Exp $
+$Id: drv_xmlproc.py,v 1.9 2000/05/12 18:41:06 lars Exp $
 """
 
-version="0.93"
+version="0.95"
 
-from xml.sax import saxlib,saxutils
+from xml.sax import saxlib,saxutils,saxmisc
 from xml.parsers.xmlproc import xmlproc
+
+import os
+
+pre_parse_properties={"http://xml.org/sax/properties/namespace-sep":1,
+                      "http://xml.org/sax/handlers/DeclHandler":1,
+                      "http://xml.org/sax/handlers/LexicalHandler":1,
+                      "http://xml.org/sax/handlers/NamespaceHandler":1}
+
+# Todo:
+# - must actually use catalog file
+# - document interplay between reset and SAX2
+# - fix bugs:
+#    - startDTD must be called
+# - do namespace processing, if it is requested
+# - support more features and properties
+
+XCATALOG =1
+SOCATALOG=2
 
 # --- SAX_XPParser
 
@@ -15,52 +33,72 @@ class SAX_XPParser(saxlib.Parser,xmlproc.Application,xmlproc.DTDConsumer,
                    xmlproc.ErrorHandler,xmlproc.PubIdResolver):
 
     def __init__(self):
-        saxlib.Parser.__init__(self)
-        self.parser=None
+	saxlib.Parser.__init__(self)
         self.reset()
+        self.declHandler=saxmisc.DeclHandler()
+        self.lexicalHandler=saxmisc.LexicalHandler()
+        self.namespaceHandler=saxmisc.NamespaceHandler()
+        self.ns_separator=" "
+        self.locator=1
+        self.is_parsing=0
+        self.stop_on_error=1
+        self.catalog_file=None
+        self.catalog_type=None
     
     def parse(self,sysID):
         self.reset()
-        self.parser.parse_resource(sysID)
+        try:
+            self.is_parsing=1
+            self.parser.parse_resource(sysID)
+        finally:
+            self.is_parsing=0
 
     def parseFile(self,file):
         self.reset()
-        self.parser.read_from(file)
-        self.parser.flush()
-        self.parser.parseEnd()
+        try:
+            self.is_parsing=1
+            self.parser.read_from(file)
+            self.parser.flush()
+            self.parser.parseEnd()
+        finally:
+            self.is_parsing=0
         
     def _create_parser(self):
-        return xmlproc.XMLProcessor()
+	return xmlproc.XMLProcessor()
 
     def setLocale(self, locale):
         try:
             self.parser.set_error_language(locale)
         except KeyError:
-            raise SAXException("Locale '%s' not supported" % locale)
+            raise saxlib.SAXNotSupportedException("Locale '%s' not supported" % locale)
         
     # --- data event methods
     
     def doc_start(self):
-        self.doc_handler.setDocumentLocator(self)
-        self.doc_handler.startDocument()
+        if self.locator:
+            self.doc_handler.setDocumentLocator(self)
+	self.doc_handler.startDocument()
 
     def doc_end(self):
-        self.doc_handler.endDocument()
+	self.doc_handler.endDocument()
 
     def handle_data(self,data,start,end):
-        self.doc_handler.characters(data,start,end-start)
+	self.doc_handler.characters(data,start,end-start)
 
     def handle_ignorable_data(self,data,start,end):
-        self.doc_handler.ignorableWhitespace(data,start,end-start)
+	self.doc_handler.ignorableWhitespace(data,start,end-start)
 
     def handle_pi(self, target, data):
-        self.doc_handler.processingInstruction(target,data)
+	self.doc_handler.processingInstruction(target,data)
 
     def handle_start_tag(self, name, attrs):
-        self.doc_handler.startElement(name,saxutils.AttributeMap(attrs))
+	self.doc_handler.startElement(name,saxutils.AttributeMap(attrs))
 
     def handle_end_tag(self, name):
-        self.doc_handler.endElement(name)
+	self.doc_handler.endElement(name)
+
+    def handle_comment(self,content):
+        self.lexicalHandler.comment(content,0,len(content))
 
     # --- pubid resolution
         
@@ -73,34 +111,56 @@ class SAX_XPParser(saxlib.Parser,xmlproc.Application,xmlproc.DTDConsumer,
     # --- error handling
 
     def warning(self,msg):
-        self.err_handler.warning(saxlib.SAXParseException(msg,None,self))
+	self.err_handler.warning(saxlib.SAXParseException(msg,None,self))
 
     def error(self,msg):
-        self.err_handler.error(saxlib.SAXParseException(msg,None,self))
+	self.err_handler.error(saxlib.SAXParseException(msg,None,self))
 
     def fatal(self,msg):
-        self.err_handler.fatalError(saxlib.SAXParseException(msg,None,self))
+	self.err_handler.fatalError(saxlib.SAXParseException(msg,None,self))
 
     # --- location handling
 
     def getColumnNumber(self):
-        return self.parser.get_column()
+	return self.parser.get_column()
 
     def getLineNumber(self):
-        return self.parser.get_line()
+	return self.parser.get_line()
 
     def getSystemId(self):
-        return self.parser.get_current_sysid()
+	return self.parser.get_current_sysid()
 
     # --- DTD parsing
 
-    def new_external_entity(self,ent_name,pub_id,sys_id,ndata):
+    def new_external_entity(self,name,pubid,sysid,ndata):
         if ndata!="":
-            self.dtd_handler.unparsedEntityDecl(ent_name,pub_id,sys_id,ndata)
+            self.dtd_handler.unparsedEntityDecl(name,pubid,sysid,ndata)
+        else:
+            # FIXME: ensure that only first decl is passed on
+            self.declHandler.externalEntityDecl(name,pubid,sysid)
 
     def new_notation(self,name,pubid,sysid):
         self.dtd_handler.notationDecl(name,pubid,sysid)
 
+    def dtd_start(self):
+	self.lexicalHandler.startDTD("","","")
+    
+    def dtd_end(self):
+	self.lexicalHandler.endDTD()
+    
+    def new_general_entity(self,name,val):
+        # FIXME: ensure that only first decl is passed on
+	self.declHandler.internalEntityDecl(name,val)
+	
+    def new_element_type(self,elem_name,elem_cont):
+        # FIXME: only first
+        self.declHandler.elementDecl(elem_name,elem_cont)
+	    
+    def new_attribute(self,elem,attr,a_type,a_decl,a_def):
+        # FIXME: only first
+        if a_decl=="#DEFAULT": a_decl=None
+        self.declHandler.attributeDecl(elem,attr,a_type,a_decl,a_def)
+        
     # --- entity events
 
     def resolve_entity(self,pubid,sysid):
@@ -128,15 +188,14 @@ class SAX_XPParser(saxlib.Parser,xmlproc.Application,xmlproc.DTDConsumer,
         return 1
 
     def reset(self):
-        if self.parser!=None:
+        if hasattr(self, "parser"):
             self.parser.deref()
-            
         self.parser=self._create_parser()
         self.parser.set_application(self)
-        self.parser.set_dtd_listener(self)
-        self.parser.set_error_handler(self)
+        self.parser.set_dtd_listener(self) # FIXME: Should we always do this?
+	self.parser.set_error_handler(self)
         self.parser.set_pubid_resolver(self)
-        self.parser.reset()
+	self.parser.reset()
     
     def feed(self,data):
         self.parser.feed(data)
@@ -144,13 +203,105 @@ class SAX_XPParser(saxlib.Parser,xmlproc.Application,xmlproc.DTDConsumer,
     def close(self):
         self.parser.close()
         self.parser.deref()
-        
         # Dereferencing to avoid circular references (grrrr)
-        self.err_handler = self.dtd_handler = self.doc_handler = None
-        self.parser = self.locator = self.ent_handler = None
+ 	self.err_handler = self.dtd_handler = self.doc_handler = None
+ 	self.parser = self.locator = self.ent_handler = None
+
+    # --- Configurable methods
+
+    def getFeature(self, featureId):
+        if featureId=="http://xml.org/sax/features/use-locator":
+            return self.locator
+        elif featureId=="http://xml.org/sax/features/validation":
+            return 0
+        elif featureId=="http://garshol.priv.no/sax/stop-on-error":
+            return self.stop_on_error
+        elif featureId=="http://garshol.priv.no/sax/use-catalog":
+            return self.catalog_file
+        elif featureId=="http://xml.org/sax/features/external-general-entities" or \
+           featureId=="http://xml.org/sax/features/external-parameter-entities" or \
+           featureId=="http://xml.org/sax/features/namespaces" or \
+           featureId=="http://xml.org/sax/features/normalize-text":
+            raise saxlib.SAXNotSupportedException("Feature %s not supported" %
+                                                  featureId)
+        else:
+            raise saxlib.SAXNotRecognizedException("Feature %s not recognized"
+                                                   % featureId)       
+
+    def setFeature(self, featureId, state):
+        if featureId=="http://xml.org/sax/features/use-locator":
+            self.locator=state
+        elif featureId=="http://garshol.priv.no/sax/stop-on-error":
+            self.stop_on_error=state
+            self.parser.set_data_after_wf_error(state)
+        elif featureId=="http://garshol.priv.no/sax/use-catalog":
+            if state:
+                if os.environ.has_key("XMLXCATALOG"):
+                    self.catalog_file=os.environ["XMLXCATALOG"]
+                    self.catalog_type=XCATALOG
+                elif os.environ.has_key("XMLSOCATALOG"):
+                    self.catalog_file=os.environ["XMLSOCATALOG"]
+                    self.catalog_type=SOCATALOG
+                else:
+                    raise saxlib.SAXException("Neither XMLXCATALOG nor "
+                                              "XMLSOCATALOG variables set")
+        elif featureId=="http://xml.org/sax/features/validation" or \
+           featureId=="http://xml.org/sax/features/external-general-entities" or \
+           featureId=="http://xml.org/sax/features/external-parameter-entities" or \
+           featureId=="http://xml.org/sax/features/namespaces" or \
+           featureId=="http://xml.org/sax/features/normalize-text":
+            raise saxlib.SAXNotSupportedException("Feature %s not supported" %
+                                                  featureId)
+        else:
+            raise saxlib.SAXNotRecognizedException("Feature %s not recognized"
+                                                   % featureId)
+
+    def getProperty(self, propertyId):
+        if propertyId=="http://xml.org/sax/properties/namespace-sep":
+            return self.ns_separator
+        elif propertyId=="http://xml.org/sax/handlers/DeclHandler":
+            return self.declHandler
+        elif propertyId=="http://xml.org/sax/handlers/LexicalHandler":
+            return self.lexicalHandler
+        elif propertyId=="http://xml.org/sax/handlers/NamespaceHandler":
+            return self.namespaceHandler
+        elif propertyId=="http://xml.org/sax/properties/dom-node" or \
+             propertyId=="http://xml.org/sax/properties/xml-string":
+            raise saxlib.SAXNotSupportedException("Property %s not supported" %
+                                                  propertyId)
+        elif propertyId=="http://garshol.priv.no/sax/xmlproc/dtdobj":
+            return self.parser.get_dtd()
+        elif propertyId=="http://garshol.priv.no/sax/catalog-file":
+            return self.catalog_file
+        else:
+            raise saxlib.SAXNotRecognizedException("Property %s not recognized"
+                                                   % propertyId)
+
+    def setProperty(self, propertyId, value):
+        if pre_parse_properties.has_key(propertyId) and self.is_parsing:
+            raise saxlib.SAXNotSupportedException("Not allowed to set "
+                                                  "property %s during parsing"
+                                                  % propertyId)
         
+        if propertyId=="http://xml.org/sax/properties/namespace-sep":
+            self.ns_separator=value
+        elif propertyId=="http://xml.org/sax/handlers/DeclHandler":
+            self.declHandler=value
+        elif propertyId=="http://xml.org/sax/handlers/LexicalHandler":
+            self.lexicalHandler=value
+        elif propertyId=="http://xml.org/sax/handlers/NamespaceHandler":
+            self.namespaceHandler=value
+        elif propertyId=="http://garshol.priv.no/sax/catalog-file":
+            self.catalog_file=value
+        elif propertyId=="http://xml.org/sax/properties/dom-node" or \
+             propertyId=="http://garshol.priv.no/sax/xmlproc/dtdobj" or \
+             propertyId=="http://xml.org/sax/properties/xml-string":
+            raise saxlib.SAXNotSupportedException("Property is read-only")
+        else:
+            raise saxlib.SAXNotRecognizedException("Property %s not recognized"
+                                                   % propertyId)
+	
 # --- Global functions
 
 def create_parser():
     return SAX_XPParser()
-    
