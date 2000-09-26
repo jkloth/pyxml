c2917f66ead3ab62ed9e51e7b1acb0a481d0c5a1
loewis <loewis>
2000-09-26 20:12:51 +0000

    Bump version to 0.6.1.

--- xml/__init__.py
+++ xml/__init__.py
@@ -2,5 +2,5 @@
 __all__ = ['dom', 'marshal', 'parsers', 'sax', 'utils']
 
 # Needs to synchronize with setup.py
-version_info = (0,6,0)
-__version__ = "0.6"
+version_info = (0,6,1)
+__version__ = "0.6.1"
