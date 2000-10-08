dcfc8d30cefd46d09c558b5aa83744a704a6479a
loewis <loewis>
2000-10-08 10:01:59 +0000

    Use the error handler.

--- xml/sax/expatreader.py
+++ xml/sax/expatreader.py
@@ -81,7 +81,9 @@ class ExpatParser(xmlreader.IncrementalParser, xmlreader.Locator):
             self._parser.Parse(data, isFinal)
         except expat.error:
             error_code = self._parser.ErrorCode
-            raise SAXParseException(expat.ErrorString(error_code), None, self)
+            exc = SAXParseException(expat.ErrorString(error_code), None, self)
+            # FIXME: when to invoke eror()?
+            self._err_handler.fatalError(exc)
 
     def close(self):
         if self._entity_stack:
