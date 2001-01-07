f532c101af06f9e3f1bc4bf521cded515d20ecc6
loewis <loewis>
2001-01-07 18:01:49 +0000

    Bump version to 0.6.4

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ else:
     xmldocfiles = []
 
 setup (name = "PyXML",
-       version = "0.6.3", # Needs to match xml/__init__.version_info
+       version = "0.6.4", # Needs to match xml/__init__.version_info
        description = "Python/XML package",
        author = "XML-SIG",
        author_email = "xml-sig@python.org",
