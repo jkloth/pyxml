1af4ebe939aea4a7b7188b5f7a43f9ea41807bd9
loewis <loewis>
2001-03-03 07:30:06 +0000

    For validating parsers, the factory needs to return parsers with validation turned on. Reported by Scott Snyder.

--- xml/sax/sax2exts.py
+++ xml/sax/sax2exts.py
@@ -1,10 +1,19 @@
 """
 Various extensions to the core SAX 2.0 API.
 
-$Id: sax2exts.py,v 1.3 2000/09/26 20:01:02 loewis Exp $
+$Id: sax2exts.py,v 1.4 2001/03/03 07:30:06 loewis Exp $
 """
 
-import saxexts
+import saxexts,saxlib
+
+# In SAX2, validation is turned-on through a property. Make sure
+# that all parsers returned from this factory are validating
+class ValidatingReaderFactory(saxexts.ParserFactory):
+    def make_parser(self, parser_list = []):
+        p = saxexts.ParserFactory.make_parser(self,parser_list)
+        p.setFeature(saxlib.feature_validation, 1)
+        return p
+
 
 # --- XMLReader factory
 
@@ -15,7 +24,7 @@ XMLReaderFactory = saxexts.ParserFactory
 XMLParserFactory = XMLReaderFactory(["xml.sax.drivers2.drv_pyexpat",
                                      "xml.sax.drivers2.drv_xmlproc"])
 
-XMLValParserFactory = XMLReaderFactory(["xml.sax.drivers2.drv_xmlproc"])
+XMLValParserFactory = ValidatingReaderFactory(["xml.sax.drivers2.drv_xmlproc"])
 
 HTMLParserFactory = XMLReaderFactory([])
 
