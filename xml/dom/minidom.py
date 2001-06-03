dcf400db69c8c02d1389354c658157a05418fdb3
loewis <loewis>
2001-06-03 14:00:24 +0000

    Compare with tag.localName in getElementsByTagNameNSHelper.

--- xml/dom/minidom.py
+++ xml/dom/minidom.py
@@ -279,7 +279,7 @@ def _getElementsByTagNameHelper(parent, name, rc):
 def _getElementsByTagNameNSHelper(parent, nsURI, localName, rc):
     for node in parent.childNodes:
         if node.nodeType == Node.ELEMENT_NODE:
-            if ((localName == "*" or node.tagName == localName) and
+            if ((localName == "*" or node.localName == localName) and
                 (nsURI == "*" or node.namespaceURI == nsURI)):
                 rc.append(node)
             _getElementsByTagNameNSHelper(node, nsURI, localName, rc)
