dec7e9ef5e4ad7f3119ad6a6fe185ba274da9c01
uche <uche>
2001-07-22 07:48:47 +0000

    Fixes Update package summary

--- xml/FtCore.py
+++ xml/FtCore.py
@@ -4,6 +4,8 @@ Contains various definitions common to modules acquired from 4Suite
 
 class FtException(Exception):
     def __init__(self, errorCode, messages, args):
+        # By defining __str__, args will be available.  Otherwise
+        # the __init__ of Exception sets it to the passed in arguments.
         self.params = args
         self.errorCode = errorCode
         self.message = messages[errorCode] % args
