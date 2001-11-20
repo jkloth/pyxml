180f24dadf328ed59f333c816a7ac4dc011a20dd
afayolle <afayolle>
2001-11-20 10:59:07 +0000

    added EMPTY_NAMESPACE

--- xml/dom/__init__.py
+++ xml/dom/__init__.py
@@ -187,6 +187,7 @@ DOMImplementation.implementation = implementation
 XML_NAMESPACE = "http://www.w3.org/XML/1998/namespace"
 XMLNS_NAMESPACE = "http://www.w3.org/2000/xmlns/"
 XHTML_NAMESPACE = "http://www.w3.org/1999/xhtml"
+EMPTY_NAMESPACE = None
 
 import MessageSource
 DOMExceptionStrings = MessageSource.__dict__['DOMExceptionStrings']
