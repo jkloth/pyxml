8c2495ae43be9f1bc1d527435456a797fc85c107
loewis <loewis>
2001-11-22 08:31:47 +0000

    Properly set __all__.

--- xml/__init__.py
+++ xml/__init__.py
@@ -1,4 +1,6 @@
-__all__ = ['dom', 'marshal', 'parsers', 'sax', 'utils']
+# xml.unicode is not listed because it is for internal use and backwards
+# compatibility only.
+__all__ = ['dom', 'marshal', 'parsers', 'sax', 'schema', 'utils', 'xpath', 'xslt']
 
 # Needs to synchronize with setup.py
 version_info = (0,7)
