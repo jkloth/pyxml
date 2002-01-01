efdd4e8b20a9019074a57c3b622cc0afa4799eae
loewis <loewis>
2002-01-01 16:44:39 +0000

    test for correct processing of ignorable whitespace

--- test/test_marshal.py
+++ test/test_marshal.py
@@ -1,5 +1,20 @@
+# built-in tests
 from xml.marshal import generic, wddx, xmlrpc
 
 generic.runtests()
 wddx.runtests()
 xmlrpc.runtests()
+
+# additional tests
+from test.test_support import verify
+
+# test for correct processing of ignorable whitespace
+data = """<?xml version="1.0"?>
+<marshal>
+  <list id="i2">
+    <int>1</int>
+    <int>2</int>
+  </list>
+</marshal>"""
+
+verify(generic.loads(data) == [1, 2])
