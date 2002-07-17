18849c5dc4341f4d4fec8f1897d56e2514e62939
fdrake <fdrake>
2002-07-17 20:38:46 +0000

    Restructure a little to make sure we clean up the temporary file created at the top.  All the imports are now at the top in "Python normal form" (standard modules first, others after).

--- test/test_xmlproc.py
+++ test/test_xmlproc.py
@@ -1,19 +1,25 @@
-from xml.parsers.xmlproc.utils import validate_doc, load_dtd, ErrorPrinter
+import os
 import sys
+
+from xml.parsers.xmlproc import xmlval
+from xml.parsers.xmlproc.utils import validate_doc, load_dtd, ErrorPrinter
 
 dtd = load_dtd("xmlval_illformed.dtd")
 
-f=open("doc.xml","w")
+f = open("doc.xml", "w")
 f.write("""<?xml version="1.0"?>
 <!DOCTYPE configuration SYSTEM "xmlval_illformed.dtd">
 <configuration><notallowed/></configuration>""")
 f.close()
 
-# validate_doc(dtd, "doc.xml")
-# validate_doc is not suitable since it prints to stderr
-from xml.parsers.xmlproc import xmlval
+try:
+    # validate_doc(dtd, "doc.xml")
+    # validate_doc is not suitable since it prints to stderr
 
-parser=xmlval.XMLValidator()
-parser.dtd=dtd # FIXME: what to do if there is a !DOCTYPE?
-parser.set_error_handler(ErrorPrinter(parser, out=sys.stdout))
-parser.parse_resource("doc.xml")
+    parser = xmlval.XMLValidator()
+    parser.dtd = dtd # FIXME: what to do if there is a !DOCTYPE?
+    parser.set_error_handler(ErrorPrinter(parser, out=sys.stdout))
+    parser.parse_resource("doc.xml")
+
+finally:
+    os.unlink("doc.xml")
