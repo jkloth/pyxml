053bd68d6b046b2b970e8d341d13bece25a8c9a7
fdrake <fdrake>
2002-07-22 21:06:17 +0000

    Minor re-organization.

--- test/test_xmlbuilder.py
+++ test/test_xmlbuilder.py
@@ -9,6 +9,15 @@ from xml.dom import XMLNS_NAMESPACE
 from xml.dom import xmlbuilder
 
 
+INTERNAL_SUBSET = "<!ELEMENT doc EMPTY>"
+DOCUMENT_SOURCE = '<!DOCTYPE doc [' + INTERNAL_SUBSET + ''']>
+<doc xmlns:a="http://xml.python.org/a"
+     xmlns:A="http://xml.python.org/a"
+     xmlns:b="http://xml.python.org/b"
+     a:a="a" b:b="b"/>
+'''
+
+
 def check_attrs(atts, expected):
     assert atts.length == len(expected)
     info = atts.itemsNS()
@@ -16,40 +25,31 @@ def check_attrs(atts, expected):
     if info != expected:
         raise AssertionError, "\n" + pprint.pformat(info)
 
+def run_checks(document, attributes):
+    source = xmlbuilder.DOMInputSource()
+    source.byteStream = StringIO(DOCUMENT_SOURCE)
+    document = builder.parse(source)
 
-INTERNAL_SUBSET = "<!ELEMENT doc EMPTY>"
-text = '<!DOCTYPE doc [' + INTERNAL_SUBSET + ''']>
-<doc xmlns:a="http://xml.python.org/a"
-     xmlns:A="http://xml.python.org/a"
-     xmlns:b="http://xml.python.org/b"
-     a:a="a" b:b="b"/>
-'''
+    if document.doctype.internalSubset != INTERNAL_SUBSET:
+        raise ValueError, "internalSubset not properly initialized"
+    check_attrs(document.documentElement.attributes, attributes)
+
 
 builder = xmlbuilder.DOMBuilder()
 builder.setFeature("namespace_declarations", 1)
 
-source = xmlbuilder.DOMInputSource()
-source.byteStream = StringIO(text)
-document = builder.parse(source)
-if document.doctype.internalSubset != INTERNAL_SUBSET:
-    raise ValueError, "internalSubset not properly initialized"
-
-check_attrs(document.documentElement.attributes,
-            [((XMLNS_NAMESPACE, "A"), "http://xml.python.org/a"),
-             ((XMLNS_NAMESPACE, "a"), "http://xml.python.org/a"),
-             ((XMLNS_NAMESPACE, "b"), "http://xml.python.org/b"),
-             (("http://xml.python.org/a", "a"), "a"),
-             (("http://xml.python.org/b", "b"), "b"),
-             ])
+run_checks(builder,
+           [((XMLNS_NAMESPACE, "A"), "http://xml.python.org/a"),
+            ((XMLNS_NAMESPACE, "a"), "http://xml.python.org/a"),
+            ((XMLNS_NAMESPACE, "b"), "http://xml.python.org/b"),
+            (("http://xml.python.org/a", "a"), "a"),
+            (("http://xml.python.org/b", "b"), "b"),
+            ])
 
 
 builder.setFeature("namespace_declarations", 0)
-source.byteStream.seek(0, 0)
-document = builder.parse(source)
-if document.doctype.internalSubset != INTERNAL_SUBSET:
-    raise ValueError, "internalSubset not properly initialized"
 
-check_attrs(document.documentElement.attributes,
-            [(("http://xml.python.org/a", "a"), "a"),
-             (("http://xml.python.org/b", "b"), "b"),
-             ])
+run_checks(builder,
+           [(("http://xml.python.org/a", "a"), "a"),
+            (("http://xml.python.org/b", "b"), "b"),
+            ])
