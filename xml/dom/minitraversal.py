7eb7603b053a2b99347e8f396254e2035847ceff
fdrake <fdrake>
2002-08-01 19:28:45 +0000

    DOMImplementation.hasFeature():  Removed, using an augmented _features     table and using the inherited implementation.
    Wrapped long lines, making the import/return constructs consistently
    phrased withing the Document class.
    
    No longer use the string module.

--- xml/dom/minitraversal.py
+++ xml/dom/minitraversal.py
@@ -1,34 +1,39 @@
 """A DOM implementation that offers traversal and ranges on top of
 minidom, using the 4DOM traversal implementation."""
 
-import minidom, string
+import minidom
 
 class DOMImplementation(minidom.DOMImplementation):
-    def hasFeature(self, feature, version):
-        if version not in ("1.0", "2.0"):
-            return 0
-        feature = string.lower(feature)
-        if feature in ['traversal','range']:
-            return 1
-        return minidom.DOMImplementation.hasFeature(self, feature, version)
+    # Augment the features table instead of duplicating the logic in
+    # hasFeature().
+    _features = minidom.DOMImplementation._features + [
+        ("traversal", "1.0"),
+        ("traversal", "2.0"),
+        ("traversal", None),
+        ("range", "1.0"),
+        ("range", "2.0"),
+        ("range", None),
+        ]
 
-    def _createDocument(self):
+    def _create_document(self):
         return Document()
 
 class Document(minidom.Document):
     implementation = DOMImplementation()
-    def createNodeIterator(self, root, whatToShow, filter, entityReferenceExpansion):
-        from xml.dom import NodeIterator
-        nodi = NodeIterator.NodeIterator(root, whatToShow, filter, entityReferenceExpansion)
-        return nodi
 
-    def createTreeWalker(self, root, whatToShow, filter, entityReferenceExpansion):
+    def createNodeIterator(self, root, whatToShow, filter,
+                           entityReferenceExpansion):
+        from xml.dom.NodeIterator import NodeIterator
+        return NodeIterator(root, whatToShow, filter, entityReferenceExpansion)
+
+    def createTreeWalker(self, root, whatToShow, filter,
+                         entityReferenceExpansion):
         from TreeWalker import TreeWalker
         return TreeWalker(root, whatToShow, filter, entityReferenceExpansion)
 
     def createRange(self):
-        import Range
-        return Range.Range(self)
+        from Range import Range
+        return Range(self)
 
 def getDOMImplementation():
     return Document.implementation
