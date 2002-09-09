bcb921500d8f8763e2412d006e6a445fe416b08a
fdrake <fdrake>
2002-09-09 18:43:36 +0000

    Only define what we can't import from the standard test.test_support module.

--- test/test_support.py
+++ test/test_support.py
@@ -1,48 +1,52 @@
-"""Partial test.test_support from Python, supports unittest-based tests."""
+"""PyUNIT-based compatibility module for the test.test_support module.
 
-import sys
+When running PyUNIT-based tests, this should be used for the
+run_suite() and run_unittest() functions.
+"""
 
-verbose = 0
+from test.test_support import verbose, TestFailed
 
-class TestFailed(Exception):
-    pass
+try:
+    from test.test_support import run_suite, run_unittest
+except ImportError:
 
-#=======================================================================
-# Preliminary PyUNIT integration.
+    #=======================================================================
+    # Preliminary PyUNIT integration.
 
-import unittest
-
-
-class BasicTestRunner:
-    def run(self, test):
-        result = unittest.TestResult()
-        test(result)
-        return result
+    import sys
+    import unittest
 
 
-def run_suite(suite, testclass=None):
-    """Run tests from a unittest.TestSuite-derived class."""
-    if verbose:
-        runner = unittest.TextTestRunner(sys.stdout, verbosity=2)
-    else:
-        runner = BasicTestRunner()
+    class BasicTestRunner:
+        def run(self, test):
+            result = unittest.TestResult()
+            test(result)
+            return result
 
-    result = runner.run(suite)
-    if not result.wasSuccessful():
-        if len(result.errors) == 1 and not result.failures:
-            err = result.errors[0][1]
-        elif len(result.failures) == 1 and not result.errors:
-            err = result.failures[0][1]
+
+    def run_suite(suite, testclass=None):
+        """Run tests from a unittest.TestSuite-derived class."""
+        if verbose:
+            runner = unittest.TextTestRunner(sys.stdout, verbosity=2)
         else:
-            if testclass is None:
-                msg = "errors occurred; run in verbose mode for details"
+            runner = BasicTestRunner()
+
+        result = runner.run(suite)
+        if not result.wasSuccessful():
+            if len(result.errors) == 1 and not result.failures:
+                err = result.errors[0][1]
+            elif len(result.failures) == 1 and not result.errors:
+                err = result.failures[0][1]
             else:
-                msg = "errors occurred in %s.%s" \
-                      % (testclass.__module__, testclass.__name__)
-            raise TestFailed(msg)
-        raise TestFailed(err)
+                if testclass is None:
+                    msg = "errors occurred; run in verbose mode for details"
+                else:
+                    msg = "errors occurred in %s.%s" \
+                          % (testclass.__module__, testclass.__name__)
+                raise TestFailed(msg)
+            raise TestFailed(err)
 
 
-def run_unittest(testclass):
-    """Run tests from a unittest.TestCase-derived class."""
-    run_suite(unittest.makeSuite(testclass), testclass)
+    def run_unittest(testclass):
+        """Run tests from a unittest.TestCase-derived class."""
+        run_suite(unittest.makeSuite(testclass), testclass)
