fd049fa2c02ebe559fd257f898ff6bcc8bc8fefd
fdrake <fdrake>
2002-09-09 16:35:26 +0000

    Update to Expat 1.95.5.

--- extensions/expat/lib/expat_config.h
+++ extensions/expat/lib/expat_config.h
@@ -50,19 +50,19 @@
 #define HAVE_UNISTD_H 1
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "expat-bugs@lists.sourceforge.net"
+#define PACKAGE_BUGREPORT "expat-bugs@mail.libexpat.org"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "expat"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "expat 1.95.4"
+#define PACKAGE_STRING "expat 1.95.5"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "expat"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.95.4"
+#define PACKAGE_VERSION "1.95.5"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
