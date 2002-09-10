f064a4663b1fafba365a8750c16ca68675546505
fdrake <fdrake>
2002-09-10 20:25:17 +0000

    Lots of cleanup, does not yet support Python 2.3 bool type directly.
    Removed several more reference leaks.
    
    boolean_print(), boolean_str():  Removed.
    
    boolean_repr():  New function.

--- extensions/boolean.c
+++ extensions/boolean.c
@@ -4,78 +4,103 @@
  See  http://4suite.org/COPYRIGHT  for license and copyright information
 */
 
-#include "boolean.h"
+#include "Python.h"
 #include <string.h>
 #include <ctype.h>
 #include <stdio.h>
 
 
-PyBooleanObject *boolean_NEW(int initval);
-static PyObject *ErrorObject = NULL;
+#if defined(_WIN32) || defined(__WIN32__)
+#include <float.h>
+#define NaN_Check(x) _isnan(x)
+#define Inf_Check(x) (_finite(x) && !_isnan(x))
+#else
+#include <math.h>
+#define NaN_Check(x) isnan(x)
+#define Inf_Check(x) isinf(x)
+#endif
 
-static PyMethodDef booleanMethods[] = {
-     { "BooleanValue", BooleanValue,   METH_VARARGS },
-     { "IsBooleanType", IsBooleanType, METH_VARARGS },
-     { NULL, NULL }
-};
+#define Boolean_Check(v)  ((v)->ob_type == &PyBoolean_Type)
+#define Boolean_Value(v)  (((PyBooleanObject *)(v))->value)
+
+typedef struct {
+    PyObject_HEAD
+    int value;
+} PyBooleanObject;
+
+static PyBooleanObject *g_true;
+static PyBooleanObject *g_false;
+
+static PyObject *g_true_string;
+static PyObject *g_false_string;
+
+static PyTypeObject PyBoolean_Type;
 
 
-static PyObject *BooleanValue(PyObject *self, PyObject *args) {
-  PyObject *obj;
-  PyObject *str_func;
-  PyBooleanObject *result = NULL;
+static PyObject *
+BooleanValue(PyObject *self, PyObject *args) {
+    PyObject *obj;
+    PyObject *str_func;
+    PyBooleanObject *result = NULL;
 
-  if (!PyArg_ParseTuple(args, "O|OO:BooleanValue", &obj, &str_func))
-    return NULL;
+    if (!PyArg_ParseTuple(args, "O|O:BooleanValue", &obj, &str_func))
+        return NULL;
 
-  if (Boolean_Check(obj)){
-    result = (PyBooleanObject *)obj;
-  } else if (PyFloat_Check(obj)) {
+    if (Boolean_Check(obj)){
+        result = (PyBooleanObject *)obj;
+    }
+    else if (PyFloat_Check(obj)) {
         if (NaN_Check(PyFloat_AS_DOUBLE(obj))) {
-          result = g_false;
-        } else {
-          result = PyObject_IsTrue(obj) ? g_true : g_false;
+            result = g_false;
         }
-  } else if (PyNumber_Check(obj) || PySequence_Check(obj)){
+        else {
+            result = PyObject_IsTrue(obj) ? g_true : g_false;
+        }
+    }
+    else if (PyNumber_Check(obj) || PySequence_Check(obj)){
         result = PyObject_IsTrue(obj) ? g_true : g_false;
-  } else if (str_func) {
-    obj = PyObject_CallFunction(str_func, "(O)", obj);
-    if (!obj) return NULL;
-    result = PyObject_IsTrue(obj) ? g_true : g_false;
-    Py_DECREF(obj);
-  } else {
-    result = g_false;
-  }
-  Py_INCREF(result);
-  return (PyObject *)result;
+    }
+    else if (str_func) {
+        obj = PyObject_CallFunction(str_func, "(O)", obj);
+        if (!obj)
+            return NULL;
+        result = PyObject_IsTrue(obj) ? g_true : g_false;
+        Py_DECREF(obj);
+    }
+    else {
+        result = g_false;
+    }
+    Py_INCREF(result);
+    return (PyObject *)result;
 }
 
-static int pyobj_as_boolean_int(PyObject *obj) {
-  if (Boolean_Check(obj)){
-    return Boolean_Value((PyBooleanObject *)obj);
-  } else if (PyNumber_Check(obj) || PySequence_Check(obj)){
-    return PyObject_IsTrue(obj) ? 1 : 0;
-  } else {
-    return 0;
-  }
+static int
+pyobj_as_boolean_int(PyObject *obj) {
+    if (Boolean_Check(obj))
+        return Boolean_Value((PyBooleanObject *)obj);
+    else if (PyNumber_Check(obj) || PySequence_Check(obj))
+        return PyObject_IsTrue(obj) ? 1 : 0;
+    else
+        return 0;
 }
 
-static PyObject *IsBooleanType(PyObject *self, PyObject *args) {
-  PyObject *obj;
-  PyObject *result = NULL;
+static PyObject *
+IsBooleanType(PyObject *self, PyObject *args) {
+    PyObject *obj;
+    PyObject *result = NULL;
 
-  if (!PyArg_ParseTuple(args, "O:IsBooleanType", &obj))
-    return NULL;
+    if (!PyArg_ParseTuple(args, "O:IsBooleanType", &obj))
+        return NULL;
 
-  if (Boolean_Check(obj))
-    result = Py_True;
-  else
-    result = Py_False;
-  Py_INCREF(result);
-  return result;
+    if (Boolean_Check(obj))
+        result = Py_True;
+    else
+        result = Py_False;
+    Py_INCREF(result);
+    return result;
 }
 
-DL_EXPORT(PyBooleanObject *)
+static PyBooleanObject *
 boolean_NEW(int initval)
 {
   PyBooleanObject *object = PyObject_NEW(PyBooleanObject, &PyBoolean_Type);
@@ -83,242 +108,210 @@ boolean_NEW(int initval)
   return object;
 }
 
-DL_EXPORT(void)
+static void
 boolean_dealloc(PyObject *self)
 {
   PyMem_DEL(self);
 }
 
-DL_EXPORT(int)
+static int
 boolean_cmp(PyObject *o1, PyObject *o2){
-  int result = -1;
-/*   int *res = (int *)malloc(sizeof(int)); */
-  PyBooleanObject *b1;
-  PyBooleanObject *b2;
+    int result = -1;
+    PyBooleanObject *b1;
+    PyBooleanObject *b2;
 
-  if (Boolean_Check(o1) && Boolean_Check(o2)) {
-    b1 = (PyBooleanObject *)o1;
-    b2 = (PyBooleanObject *)o2;
-    result = !(Boolean_Value(o1) == Boolean_Value(o2));
-  } else if (Boolean_Check(o1)) {
-    b1 = (PyBooleanObject *)o1;
-    result = !(Boolean_Value(o1) == pyobj_as_boolean_int(o2));
-  } else if (Boolean_Check(o2)) {
-    b2 = (PyBooleanObject *)o2;
-    result = !(Boolean_Value(o2) == pyobj_as_boolean_int(o1));
-  }
-  return result;
+    if (Boolean_Check(o1) && Boolean_Check(o2)) {
+        b1 = (PyBooleanObject *)o1;
+        b2 = (PyBooleanObject *)o2;
+        result = !(Boolean_Value(o1) == Boolean_Value(o2));
+    }
+    else if (Boolean_Check(o1)) {
+        b1 = (PyBooleanObject *)o1;
+        result = !(Boolean_Value(o1) == pyobj_as_boolean_int(o2));
+    }
+    else if (Boolean_Check(o2)) {
+        b2 = (PyBooleanObject *)o2;
+        result = !(Boolean_Value(o2) == pyobj_as_boolean_int(o1));
+    }
+    return result;
 }
 
-static PyObject *boolean_str(PyObject *self)
+static PyObject *
+boolean_repr(PyObject *self)
 {
-  PyObject *result = NULL;
-  PyBooleanObject *obj = (PyBooleanObject *)self;
+    PyObject *result;
 
-  if (Boolean_Value(obj)){
-    result = PyString_FromString("true");
-  } else {
-    result = PyString_FromString("false");
-  }
-  Py_INCREF(result);
-  return result;
+    if (Boolean_Value((PyBooleanObject *)self))
+        result = g_true_string;
+    else
+        result = g_false_string;
+    Py_INCREF(result);
+    return result;
 }
 
-static int boolean_print(PyObject *self, FILE *fp, int flags)
+static int
+boolean_coerce(PyObject **v, PyObject **w)
 {
-  char buf[256];
-  PyBooleanObject *obj = (PyBooleanObject *)self;
+    PyObject *newv, *neww;
 
-  /*Yeah, wasteful, but I hevan't yet decided whether to add to the output*/
-  sprintf(buf, "%s", Boolean_Value(obj) ? "true" : "false");
-  fputs(buf, fp);
-  return 0;
+    if ((*v)->ob_type == (*w)->ob_type){
+        Py_INCREF(*v);
+        Py_INCREF(*w);
+        return 0;
+    }
+    newv = PyNumber_Int(*v);
+    neww = PyNumber_Int(*w);
+    if (newv && neww){
+        *v = newv;
+        *w = neww;
+        return 0;
+    }
+    Py_XDECREF(newv);
+    Py_XDECREF(neww);
+    return -1;  /* couldn't do it */
 }
 
-static int boolean_coerce(PyObject **v, PyObject **w){
-  PyObject *newv, *neww;
+static PyObject *
+boolean_and(PyObject *o1, PyObject *o2)
+{
+    /* FIXME: Check whether we need to conver the 1st arg.
+       The Python/C docs don't help */
+    int lhs = pyobj_as_boolean_int(o1), rhs = pyobj_as_boolean_int(o2);
+    PyObject *result = NULL;
 
-  if ((*v)->ob_type == (*w)->ob_type){
-    Py_INCREF(*v);
-    Py_INCREF(*w);
-    return 0;
-  }
-  newv = PyNumber_Int(*v);
-  neww = PyNumber_Int(*w);
-  if (newv && neww){
-    Py_INCREF(newv);
-    Py_INCREF(neww);
-    *v = newv;
-    *w = neww;
-    return 0;
-  }
-  return -1;  /* couldn't do it */
+    result = PyInt_FromLong((long)(lhs && rhs));
+    Py_INCREF(result);
+    return result;
 }
 
-static PyObject *boolean_and(PyObject *o1, PyObject *o2){
-  /* FIXME: Check whether we need to conver the 1st arg.  The Python/C docs don't help */
-  int lhs = pyobj_as_boolean_int(o1), rhs = pyobj_as_boolean_int(o2);
-  PyObject *result = NULL;
+static PyObject *
+boolean_or(PyObject *o1, PyObject *o2)
+{
+    /* FIXME: Check whether we need to conver the 1st arg.
+       The Python/C docs don't help */
+    int lhs = pyobj_as_boolean_int(o1), rhs = pyobj_as_boolean_int(o2);
 
-  result = PyInt_FromLong((long)(lhs && rhs));
-  Py_INCREF(result);
-  return result;
+    return PyInt_FromLong((long)(lhs || rhs));
 }
 
-static PyObject *boolean_or(PyObject *o1, PyObject *o2){
-  /* FIXME: Check whether we need to conver the 1st arg.  The Python/C docs don't help */
-  int lhs = pyobj_as_boolean_int(o1), rhs = pyobj_as_boolean_int(o2);
-  PyObject *result = NULL;
+static PyObject *
+boolean_xor(PyObject *o1, PyObject *o2)
+{
+    /* FIXME: Check whether we need to conver the 1st arg.
+       The Python/C docs don't help */
+    int lhs = pyobj_as_boolean_int(o1), rhs = pyobj_as_boolean_int(o2);
 
-  result = PyInt_FromLong((long)(lhs || rhs));
-  Py_INCREF(result);
-  return result;
+    return PyInt_FromLong((long)(lhs ^ rhs));
 }
 
-static PyObject *boolean_xor(PyObject *o1, PyObject *o2){
-  /* FIXME: Check whether we need to conver the 1st arg.  The Python/C docs don't help */
-  int lhs = pyobj_as_boolean_int(o1), rhs = pyobj_as_boolean_int(o2);
-  PyObject *result = NULL;
-
-  result = PyInt_FromLong((long)(lhs ^ rhs));
-  Py_INCREF(result);
-  return result;
+static int
+boolean_nonzero(PyObject *o)
+{
+    return Boolean_Value((PyBooleanObject *)o);
 }
 
-static int boolean_nonzero(PyObject *o){
-  PyBooleanObject *obj = (PyBooleanObject *)o;
+static PyObject *
+boolean_int(PyObject *o)
+{
+    PyBooleanObject *obj = (PyBooleanObject *)o;
 
-  return Boolean_Value(obj);
+    return PyInt_FromLong((long)Boolean_Value(obj));
 }
 
-static PyObject *boolean_int(PyObject *o){
-  PyBooleanObject *obj = (PyBooleanObject *)o;
-  PyObject *result = NULL;
+static PyObject *
+boolean_long(PyObject *o)
+{
+    PyBooleanObject *obj = (PyBooleanObject *)o;
 
-  result = PyInt_FromLong((long)Boolean_Value(obj));
-  Py_INCREF(result);
-  return result;
+    return PyLong_FromLong((long)Boolean_Value(obj));
 }
 
-static PyObject *boolean_long(PyObject *o){
-  PyBooleanObject *obj = (PyBooleanObject *)o;
-  PyObject *result = NULL;
+static PyObject *
+boolean_float(PyObject *o)
+{
+    PyBooleanObject *obj = (PyBooleanObject *)o;
 
-  result = PyLong_FromLong((long)Boolean_Value(obj));
-  Py_INCREF(result);
-  return result;
-}
-
-static PyObject *boolean_float(PyObject *o){
-  PyBooleanObject *obj = (PyBooleanObject *)o;
-  PyObject *result = NULL;
-
-  result = PyFloat_FromDouble((double)Boolean_Value(obj));
-  Py_INCREF(result);
-  return result;
-}
-
-DL_EXPORT(void) 
-initboolean(void) {
-  PyObject *m, *d;
-
-  m = Py_InitModule("boolean", booleanMethods);
-  d = PyModule_GetDict(m);
-
-  PyBoolean_Type.ob_type = &PyType_Type;
-  PyDict_SetItemString(d, "BooleanType", (PyObject *)&PyBoolean_Type);
-  Py_INCREF(&PyBoolean_Type);
-
-  ErrorObject = PyString_FromString("boolean.error");
-  PyDict_SetItemString(d, "error", ErrorObject);
-
-  g_true = boolean_NEW(1);
-  Py_INCREF(g_true);
-
-  g_false = boolean_NEW(0);
-  Py_INCREF(g_false);
-
-  PyDict_SetItemString(d, "true", (PyObject *)g_true);
-  PyDict_SetItemString(d, "false", (PyObject *)g_false);
-
-  return;
+    return PyFloat_FromDouble((double)Boolean_Value(obj));
 }
 
 static PyNumberMethods boolean_as_number = {
-  0,                 /* binaryfunc nb_add;       __add__ */
-  0,                 /* binaryfunc nb_subtract;  __sub__ */
-  0,                 /* binaryfunc nb_multiply;  __mul__ */
-  0,                 /* binaryfunc nb_divide;    __div__ */
-  0,                 /* binaryfunc nb_remainder; __mod__ */
-  0,                 /* binaryfunc nb_divmod;    __divmod__ */
-  0,                 /* ternaryfunc nb_power;    __pow__ */
-  0,                 /* unaryfunc nb_negative;   __neg__ */
-  0,                 /* unaryfunc nb_positive;   __pos__ */
-  0,                 /* unaryfunc nb_absolute;   __abs__ */
-  boolean_nonzero,   /* inquiry nb_nonzero;      __nonzero__ */
-  0,                 /* unaryfunc nb_invert;     __invert__ */
-  0,                 /* binaryfunc nb_lshift;    __lshift__ */
-  0,                 /* binaryfunc nb_rshift;    __rshift__ */
-  boolean_and,       /* binaryfunc nb_and;       __and__ */
-  boolean_xor,       /* binaryfunc nb_xor;       __xor__ */
-  boolean_or,        /* binaryfunc nb_or;        __or__ */
-  boolean_coerce,    /* coercion nb_coerce;      __coerce__ */
-  boolean_int,       /* unaryfunc nb_int;        __int__ */
-  boolean_long,      /* unaryfunc nb_long;       __long__ */
-  boolean_float,     /* unaryfunc nb_float;      __float__ */
-  0,                 /* unaryfunc nb_oct;        __oct__ */
-  0,                 /* unaryfunc nb_hex;        __hex__ */
+    0,                 /* binaryfunc nb_add;       __add__ */
+    0,                 /* binaryfunc nb_subtract;  __sub__ */
+    0,                 /* binaryfunc nb_multiply;  __mul__ */
+    0,                 /* binaryfunc nb_divide;    __div__ */
+    0,                 /* binaryfunc nb_remainder; __mod__ */
+    0,                 /* binaryfunc nb_divmod;    __divmod__ */
+    0,                 /* ternaryfunc nb_power;    __pow__ */
+    0,                 /* unaryfunc nb_negative;   __neg__ */
+    0,                 /* unaryfunc nb_positive;   __pos__ */
+    0,                 /* unaryfunc nb_absolute;   __abs__ */
+    boolean_nonzero,   /* inquiry nb_nonzero;      __nonzero__ */
+    0,                 /* unaryfunc nb_invert;     __invert__ */
+    0,                 /* binaryfunc nb_lshift;    __lshift__ */
+    0,                 /* binaryfunc nb_rshift;    __rshift__ */
+    boolean_and,       /* binaryfunc nb_and;       __and__ */
+    boolean_xor,       /* binaryfunc nb_xor;       __xor__ */
+    boolean_or,        /* binaryfunc nb_or;        __or__ */
+    boolean_coerce,    /* coercion nb_coerce;      __coerce__ */
+    boolean_int,       /* unaryfunc nb_int;        __int__ */
+    boolean_long,      /* unaryfunc nb_long;       __long__ */
+    boolean_float,     /* unaryfunc nb_float;      __float__ */
+    0,                 /* unaryfunc nb_oct;        __oct__ */
+    0,                 /* unaryfunc nb_hex;        __hex__ */
 };
 
-PyTypeObject PyBoolean_Type = {
+static PyTypeObject PyBoolean_Type = {
     PyObject_HEAD_INIT(0)
     0,
     "boolean",
     sizeof(PyBooleanObject),
     0,
-    0,                    /* tp_dealloc */
-    boolean_print,        /* tp_print */
+    boolean_dealloc,      /* tp_dealloc */
+    0,                    /* tp_print */
     0,                    /* tp_getattr */
     0,                    /* tp_setattr */
     (cmpfunc)boolean_cmp, /* tp_compare */
-    0,                    /* tp_repr */
+    boolean_repr,         /* tp_repr */
     &boolean_as_number,   /* tp_as_number */
     0,                    /* tp_as_sequence */
     0,                    /* tp_as_mapping */
     0,                    /* tp_hash */
     0,                    /* tp_call */
-    boolean_str,          /* tp_str */
+    0,                    /* tp_str */
     0,                    /* tp_getattro */
     0,                    /* tp_setattro */
 };
 
-
-#if (0)
-
-static PyNumberMethods boolean_as_number = {
-  boolean_add,       /* binaryfunc nb_add;         __add__ */
-  boolean_sub,       /* binaryfunc nb_subtract;    __sub__ */
-  boolean_mul,       /* binaryfunc nb_multiply;    __mul__ */
-  boolean_div,       /* binaryfunc nb_divide;      __div__ */
-  boolean_mod,       /* binaryfunc nb_remainder;   __mod__ */
-  boolean_divmod,    /* binaryfunc nb_divmod;      __divmod__ */
-  boolean_pow,       /* ternaryfunc nb_power;      __pow__ */
-  boolean_neg,       /* unaryfunc nb_negative;     __neg__ */
-  boolean_pos,       /* unaryfunc nb_positive;     __pos__ */
-  boolean_abs,       /* unaryfunc nb_absolute;     __abs__ */
-  boolean_nonzero,   /* inquiry nb_nonzero;        __nonzero__ */
-  boolean_invert,    /* unaryfunc nb_invert;       __invert__ */
-  boolean_lshift,    /* binaryfunc nb_lshift;      __lshift__ */
-  boolean_rshift,    /* binaryfunc nb_rshift;      __rshift__ */
-  boolean_and,       /* binaryfunc nb_and;         __and__ */
-  boolean_xor,       /* binaryfunc nb_xor;         __xor__ */
-  boolean_or,        /* binaryfunc nb_or;          __or__ */
-  boolean_coerce,    /* coercion nb_coerce;        __coerce__ */
-  boolean_int,       /* unaryfunc nb_int;          __int__ */
-  boolean_long,      /* unaryfunc nb_long;         __long__ */
-  boolean_float,     /* unaryfunc nb_float;        __float__ */
-  boolean_oct,       /* unaryfunc nb_oct;          __oct__ */
-  boolean_hex,       /* unaryfunc nb_hex;          __hex__ */
+static PyMethodDef booleanMethods[] = {
+     { "BooleanValue",  BooleanValue,  METH_VARARGS },
+     { "IsBooleanType", IsBooleanType, METH_VARARGS },
+     { NULL, NULL }
 };
 
-#endif
+DL_EXPORT(void)
+initboolean(void) {
+  PyObject *m;
+
+  m = Py_InitModule("boolean", booleanMethods);
+
+  PyBoolean_Type.ob_type = &PyType_Type;
+  Py_INCREF(&PyBoolean_Type);
+  PyModule_AddObject(m, "BooleanType", (PyObject *)&PyBoolean_Type);
+
+  if (g_true_string == NULL)
+      g_true_string = PyString_FromString("true");
+  if (g_false_string == NULL)
+      g_false_string = PyString_FromString("false");
+
+  if (g_true == NULL)
+      g_true = boolean_NEW(1);
+  if (g_false == NULL)
+      g_false = boolean_NEW(0);
+
+  Py_INCREF(g_true);
+  PyModule_AddObject(m, "true", (PyObject *)g_true);
+  Py_INCREF(g_false);
+  PyModule_AddObject(m, "false", (PyObject *)g_false);
+
+  return;
+}
