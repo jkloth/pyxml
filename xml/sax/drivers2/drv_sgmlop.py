7464f128460d9b0a2630305a3bc4a8996c7aeda2
loewis <loewis>
2003-01-21 12:42:28 +0000

    Patch #644843: Report skippedEntity() as Unicode objects.

--- xml/sax/drivers2/drv_sgmlop.py
+++ xml/sax/drivers2/drv_sgmlop.py
@@ -1,7 +1,7 @@
 """
 SAX2 driver for the sgmlop parser.
 
-$Id: drv_sgmlop.py,v 1.6 2002/05/30 09:13:43 syt Exp $
+$Id: drv_sgmlop.py,v 1.7 2003/01/21 12:42:28 loewis Exp $
 """
 
 version = "0.1"
@@ -93,7 +93,7 @@ class SaxParser(SGMLParser, XMLReader):
         self._cont_handler.characters(to_xml_string(data,self._encoding))
 
     def unknown_entityref(self, entity):
-        self._cont_handler.skippedEntity(entity)
+        self._cont_handler.skippedEntity(to_xml_string(entity,self._encoding))
 
     def handle_comment(self,data):
         if self._lexical_handler is not None:
